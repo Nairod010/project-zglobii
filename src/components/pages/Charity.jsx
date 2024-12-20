f355da8764a220c1dd8d02c33eb2969263728d6e
Clara Peru <blegu.dorian@gmail.com>
2024-12-20 22:54:35 +0200

    second webshit test

--- src/components/pages/Charity.jsx
+++ src/components/pages/Charity.jsx
@@ -1,8 +1,6 @@
 export default function Charity() {
   return (
     <h1>Evenimente Caritabile</h1>
-    <div className="test">
-      test
-    </div>
+    <div className={"check"}> testtt</div>
   )
 }

76f6e545c18b0259792688e8b338869ebce5d023
Clara Peru <blegu.dorian@gmail.com>
2024-12-20 22:46:42 +0200

    test

--- src/components/pages/Charity.jsx
+++ src/components/pages/Charity.jsx
@@ -1,3 +1,8 @@
 export default function Charity() {
-  return <h1>Evenimente Caritabile</h1>
+  return (
+    <h1>Evenimente Caritabile</h1>
+    <div className="test">
+      testttttt 
+    </div>
+  )
 }
